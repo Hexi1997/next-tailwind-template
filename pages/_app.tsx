f145de342cc2b1034c71c31d7bff30af59a25400
牛叠谊 <18715156450@163.com>
2022-03-27 12:01:24 +0800

    feat: add router change loading bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,21 +4,29 @@ import '../styles/globals.css';
 
 import { BaseProvider } from 'baseui';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import { appWithTranslation } from 'next-i18next';
 import { ThemeProvider } from 'next-themes';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Provider as StyletronProvider } from 'styletron-react';
 
+import { LoadingBar } from '@/components/Common/LoadingBar';
 import { Layout } from '@/components/Layout';
 import { theme } from '@/styles/baseuiTheme';
 import { styletron } from '@/styletron';
+import useRouterChange from '@/utils/hooks/useRouterChange';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { state } = useRouterChange();
   return (
     <ThemeProvider attribute="class">
       <StyletronProvider value={styletron}>
         <BaseProvider theme={theme}>
           <Layout>
+            <LoadingBar
+              isRouteChanging={state.isRouteChanging}
+              key={state.loadingKey}
+            />
             <Component {...pageProps} />
           </Layout>
         </BaseProvider>
