6838a81651dabc2d46c1534c11506011a05441e3
WKlili <18136225579@163.com>
2022-04-01 15:39:21 +0800

    update config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  target: 'experimental-serverless-trace',
   reactStrictMode: true,
   i18n,
   async redirects() {
