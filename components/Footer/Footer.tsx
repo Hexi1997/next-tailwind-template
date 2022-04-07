586e0c573eb91a7b274beca6fa62e7cfe609ac76
Poem7200 <564650685@qq.com>
2022-04-07 15:31:17 +0800

    refactor: footer style for portable devices

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,6 +2,8 @@ import cn from 'classnames';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useTranslation } from 'next-i18next';
+import { useMemo } from 'react';
+import { useWindowSize } from 'react-use';
 
 import discord from '@/assets/images/footer/discord.svg';
 import medium from '@/assets/images/footer/medium.svg';
@@ -14,103 +16,143 @@ interface FooterProps {
   className?: string;
 }
 
+const contactAppList = [
+  {
+    id: 1,
+    app: twitter,
+    link: globalConfig.socialLinks.twitter
+  },
+  {
+    id: 2,
+    app: medium,
+    link: globalConfig.socialLinks.medium
+  },
+  {
+    id: 3,
+    app: discord,
+    link: globalConfig.socialLinks.discord
+  }
+];
+const menuList = [
+  {
+    id: 1,
+    title: 'MENU_COLLECTIONS',
+    link: '/collections'
+  },
+  {
+    id: 2,
+    title: 'MENU_MARKET',
+    link: '/market'
+  }
+];
+
 export function Footer(props: FooterProps) {
   const { className } = props;
   const { t } = useTranslation(['common', 'menu']);
+  const { width } = useWindowSize();
+  const isMobile = useMemo(() => {
+    return width < 1024;
+  }, [width]);
 
   return (
     <div
       className={cn(
         styles.Footer,
         className,
-        'bg-[#29a654] px-[75px] text-white',
-        'text-center',
-        'lg:text-right'
+        'lg:px-[75px]',
+        'bg-[#29a654] text-white'
       )}
     >
+      {/* PC端：左右排列，移动端：上下反向排列 */}
       <div
         className={cn(
-          'pt-10 text-xl font-normal',
-          'lg:mb-0 lg:block lg:text-left',
-          'mb-12 flex items-center justify-center text-center'
+          'flex',
+          'lg:flex-row lg:items-end lg:justify-between lg:pb-6',
+          'flex flex-col-reverse px-4'
         )}
       >
-        <div
-          className={cn(
-            'h-[50px] w-[50px] rounded-[50%] bg-white',
-            'lg:mb-4',
-            'mr-4'
-          )}
-        ></div>
-        {t('COMMON_MARKET_NAME')}
-      </div>
-      <div
-        className={cn(
-          'mb-6 flex justify-between',
-          'lg:flex-row lg:items-end',
-          'flex-col'
-        )}
-      >
-        <div
-          className={cn(
-            'grid translate-y-[-50%] items-center justify-items-center gap-x-20 text-[24px] font-black leading-[24px]',
-            'lg:my-0',
-            'sm:my-8 sm:grid-cols-3 sm:grid-rows-1',
-            'gap-y-6',
-            'mt-10'
-          )}
-        >
-          <Link href="/collections">
-            {t('MENU_COLLECTIONS', { ns: 'menu' })}
-          </Link>
-          <Link href="/market">{t('MENU_MARKET', { ns: 'menu' })}</Link>
-        </div>
-        <div className={cn('lg:block', 'flex items-center justify-center')}>
-          <p
-            className={cn(
-              'text-xl font-extralight',
-              'lg:mb-[20px] lg:mr-0',
-              'mr-[32px]'
-            )}
-          >
-            {t('COMMON_CONTACT_US')}
-          </p>
+        <div className={cn('lg:pt-10', 'pt-3')}>
           <div
             className={cn(
-              'item-center grid grid-cols-3 grid-rows-1 justify-between gap-x-[32px]'
+              'mb-4 h-[50px] w-[50px] rounded-[50%] bg-white',
+              'lg:block',
+              'hidden'
+            )}
+          ></div>
+          <div
+            className={cn(
+              'lg:text-left lg:text-xl lg:font-semibold',
+              'text-center text-xs font-semibold'
             )}
           >
-            <a
-              href={globalConfig.socialLinks.twitter}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="flex h-[44px] w-[44px] cursor-pointer justify-center rounded-[16px] bg-white align-middle">
-                <Image src={twitter} />
-              </div>
-            </a>
-            <a
-              href={globalConfig.socialLinks.medium}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="flex h-[44px] w-[44px] cursor-pointer justify-center rounded-[16px] bg-white align-middle">
-                <Image src={medium} />
-              </div>
-            </a>
-            <a
-              href={globalConfig.socialLinks.discord}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="flex h-[44px] w-[44px] cursor-pointer justify-center rounded-[16px] bg-white align-middle">
-                <Image src={discord} />
-              </div>
-            </a>
+            {t('COMMON_MARKET_NAME')}
+          </div>
+          <div
+            className={cn(
+              'grid grid-cols-2',
+              'lg:mt-12 lg:justify-items-start lg:gap-20 lg:pb-2',
+              'mt-3 justify-items-center gap-6 pb-4'
+            )}
+          >
+            {menuList.map((item) => (
+              <Link key={item.id} href={item.link}>
+                <span
+                  className={cn(
+                    'font-black hover:cursor-pointer',
+                    'lg:text-2xl',
+                    'text-sm'
+                  )}
+                >
+                  {t(item.title, { ns: 'menu' })}
+                </span>
+              </Link>
+            ))}
           </div>
         </div>
+        <div className={cn('lg:pt-0 lg:text-right', 'pt-4 text-center')}>
+          <div className={cn('lg:mb-5 lg:text-[22px]', 'mb-3 text-sm')}>
+            {t('COMMON_CONTACT_US')}
+          </div>
+          <div
+            className={cn(
+              'grid grid-cols-3 justify-items-center gap-6',
+              'lg:px-0',
+              'px-28'
+            )}
+          >
+            {contactAppList.map((item) => (
+              <a
+                key={item.id}
+                href={item.link}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div
+                  className={cn(
+                    'flex items-center justify-center bg-white',
+                    'lg:h-11 lg:w-11 lg:rounded-2xl',
+                    'h-6 w-6 rounded-lg'
+                  )}
+                >
+                  <Image
+                    src={item.app}
+                    width={isMobile ? 9 : 19}
+                    height={isMobile ? 11 : 22}
+                  />
+                </div>
+              </a>
+            ))}
+          </div>
+          <div className={cn('mt-4 h-[1px] bg-white', 'lg:hidden')}></div>
+        </div>
       </div>
-      <div className="border-t-[1px] border-solid border-white pt-[20px] pb-[30px] text-base">
+      <div className={cn('h-[1px] bg-white', 'lg:block', 'hidden')}></div>
+      <div
+        className={cn(
+          'lg:bg-[#29a654] lg:pt-5 lg:pb-8 lg:text-right lg:text-base lg:text-white',
+          'bg-white py-1 text-center text-xs text-[#29a654]'
+        )}
+      >
         Copyright 2022 Matrix Labs Design &amp; Development
       </div>
     </div>
