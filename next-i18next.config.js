04489f37a7f2adab869b0e384230515ad87b8b24
牛叠谊 <18715156450@163.com>
2022-09-20 16:13:09 +0800

    feat: change i18n folder name

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,9 +3,6 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'zh'],
-    localePath: path.join(
-      __dirname,
-      './public/flowmarket-frontend-copywriter/locales'
-    )
+    localePath: path.join(__dirname, './public/i18n/locales')
   }
 };
