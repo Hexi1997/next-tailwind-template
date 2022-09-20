196ec8b5e9f9f17797b2ae4f07be6d45759131b3
牛叠谊 <18715156450@163.com>
2022-09-20 15:42:09 +0800

    feat: add multi env config

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -12,6 +12,15 @@ export function Header(props: HeaderProps) {
   const { t } = useTranslation('common');
 
   return (
-    <div className={cn(styles.Header, className)}>{t('COMMON_LANGUAGE')}</div>
+    <div className={cn(styles.Header, className)}>
+      <div>
+        <span>language: </span>
+        {t('COMMON_LANGUAGE')}
+      </div>
+      <div>
+        <span>env: </span>
+        {process.env.NEXT_PUBLIC_ENV_NAME}
+      </div>
+    </div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,27 +1,47 @@
+const { i18n } = require('./next-i18next.config.js');
 const path = require('path');
-const { i18n } = require('./next-i18next.config');
+const yenv = require('yenv');
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  i18n,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')]
-  },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: true
-      },
-      {
-        source: '/collections',
-        destination: '/collections/all',
-        permanent: true
-      }
-    ];
-  }
+function getEnvVars(rawEnv, dev) {
+  const NEXT_PUBLIC = /^NEXT_PUBLIC_/i;
+  const raw = Object.keys(rawEnv)
+    .filter((key) => NEXT_PUBLIC.test(key))
+    .reduce((env, key) => {
+      env[key] = rawEnv[key];
+      return env;
+    }, {});
+  // Stringify all values so we can feed into Webpack DefinePlugin
+  const stringified = {
+    'process.env': Object.keys(raw).reduce((env, key) => {
+      env[key] = JSON.stringify(raw[key]);
+      return env;
+    }, {})
+  };
+  return { raw, stringified };
+}
+
+module.exports = (phase) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+  const NEXT_ENV = isDev ? 'alpha' : process.env.APP_ENV;
+  const rawEnv = yenv(path.resolve('.env.yml'), { raw: true, env: NEXT_ENV });
+
+  return {
+    target: 'experimental-serverless-trace',
+    reactStrictMode: true,
+    images: {
+      domains: []
+    },
+    i18n,
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/home',
+          permanent: true
+        }
+      ];
+    },
+    env: getEnvVars(rawEnv, isDev).raw
+  };
 };
-
-module.exports = nextConfig;
