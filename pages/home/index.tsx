6a3ffef57c2d6d7016fd7b15a160451ac8fc4645
牛叠谊 <18715156450@163.com>
2022-09-20 15:53:33 +0800

    feat: add change locale demo

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,6 +1,8 @@
 import cn from 'classnames';
 import { GetStaticPropsContext } from 'next';
+import { useRouter } from 'next/router';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useCallback } from 'react';
 
 import styles from './_index.module.scss';
 
@@ -18,8 +20,26 @@ export async function getStaticProps({ locale }: GetStaticPropsContext) {
 
 function Home(props: homeProps) {
   const { className } = props;
+  const router = useRouter();
 
-  return <div className={cn(styles.home, className)}>home</div>;
+  const handleLocaleChange = useCallback(() => {
+    router
+      .replace(router.asPath, router.asPath, {
+        locale: router.locale === 'zh' ? 'en' : 'zh'
+      })
+      .catch(console.error);
+  }, [router]);
+
+  return (
+    <div className={cn(styles.home, className)}>
+      <button
+        onClick={handleLocaleChange}
+        className="w-40 h-10 bg-gray-500 text-white"
+      >
+        change language
+      </button>
+    </div>
+  );
 }
 
 export default Home;
